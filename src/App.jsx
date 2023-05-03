d9120a78107fcd7233502b87e523ff03ab606574
Joseph Martinez <joseph.r.martinez@gmail.com>
2023-05-03 12:44:09 -0700

    Implement getHealthyRecipe function

--- src/App.jsx
+++ src/App.jsx
@@ -176,6 +176,35 @@ function App() {
       });
   }
 
+  function getHealthyRecipe() {
+    setLoading(true);
+
+    // Submit prompt to openAI API
+    const prompt = `Rewrite this recipe to be healthier: ${JSON.stringify(recipe)} Format response as: {"dish": ${dishName}, "ingredients": [array of strings],
+    "instructions": [array of strings]}`;
+
+    openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+    })
+      .then((completion) => {
+        // Handle API response
+        const generatedText =
+          completion.data.choices[0].message.content;
+
+        console.log(completion);  
+        console.log(generatedText);
+        setLoading(false)
+        setRecipe(JSON.parse(generatedText));
+        setUserInput("")
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false)
+        setRecipe("");
+      });
+  }
+
 
   return (
     <div className='flex flex-col items-center'>
