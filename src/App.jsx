203f0f9f9a2e7265da1c864f0683e0bb0eaaadc5
Joseph Martinez <joseph.r.martinez@gmail.com>
2023-05-02 17:31:24 -0700

    Utilize .env instead of firebase to store API key

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+// import { openaiKey } from '../firebase'
 import { useState } from 'react'
 import './App.css'
-// import { openaiKey } from '../firebase'
 import { db } from '../firebase'
 import { collection, addDoc, doc, getDoc } from "firebase/firestore";
 import { Configuration, OpenAIApi } from "openai";
 
+
+
+const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
 
 function App() {
   const [recipe, setRecipe] = useState({})
@@ -12,7 +15,6 @@ function App() {
   const [selectedIngredient, setSelectedIngredient] = useState("")
   const [userInput, setUserInput] = useState("")
   const [loading, setLoading] = useState(false)
-  
 
   let dishName = ""
   let ingredients = []
@@ -35,6 +37,16 @@ function App() {
     })
   }
   
+  
+  function checkForSubmit(event) {
+    if (userInput.trim().length < 3) return;
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      getRecipe()
+    }
+  }
+  
+
   function togglePopup() {
     setPopup(!popup)
   }
@@ -44,43 +56,30 @@ function App() {
     togglePopup()
   }
 
-  function getRecipe(e) {
-    e.preventDefault();
+  function getRecipe() {
     setLoading(true);
 
-    // Get api key
-    const docRef = doc(db, "api-keys", "openai-api-key");
-    getDoc(docRef)
-      .then((docSnap) => {
-        const key = docSnap.data();
-        const openaiKey = key.key;
+    // Submit prompt to openAI API
+    const configuration = new Configuration({
+      apiKey: apiKey,
+    });         
+    const openai = new OpenAIApi(configuration);
+    const prompt = `return a recipe for ${userInput} formatted as: {"dish": ${userInput}, "ingredients": [array of strings],
+    "instructions": [array of strings]}`;
 
-        const configuration = new Configuration({
-          apiKey: openaiKey,
-        });         
+    openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+    })
+      .then((completion) => {
+        // Handle API response
+        const generatedText =
+          completion.data.choices[0].message.content;
 
-        const openai = new OpenAIApi(configuration);
-        const prompt = `return a recipe for ${userInput} formatted as: {"dish": ${userInput}, "ingredients": [array of strings],
-        "instructions": [array of strings]}`;
-
-        openai.createChatCompletion({
-          model: "gpt-3.5-turbo",
-          messages: [{ role: "user", content: prompt }],
-        })
-          .then((completion) => {
-            const generatedText =
-              completion.data.choices[0].message.content;
-
-            console.log(completion);  
-            console.log(generatedText);
-            setLoading(false)
-            setRecipe(JSON.parse(generatedText));
-          })
-          .catch((error) => {
-            console.log(error);
-            setLoading(false)
-            setRecipe("");
-          });
+        console.log(completion);  
+        console.log(generatedText);
+        setLoading(false)
+        setRecipe(JSON.parse(generatedText));
       })
       .catch((error) => {
         console.log(error);
@@ -96,16 +95,17 @@ function App() {
         type="text"
         className='input input-bordered max-w-xs'
         value={userInput}
+        onKeyDown={checkForSubmit}
         onChange={(e) => setUserInput(e.target.value.toLowerCase())}/>
       <button className='btn btn-primary my-6'
         onClick={getRecipe}>get recipe</button> 
       {loading && 
         <div role="status">
-        <svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-slate-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg aria-hidden="true" className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-slate-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
             <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
         </svg>
-        <span class="sr-only">Loading...</span>
+        <span className="sr-only">Loading...</span>
         </div>
         }
       {instructions.length > 1 && <div className=''>
@@ -127,3 +127,12 @@ function App() {
 }
 
 export default App
+
+
+
+// Get api key from firebase
+    // const docRef = doc(db, "api-keys", "openai-api-key");
+    // getDoc(docRef)
+    //   .then((docSnap) => {
+    //     const key = docSnap.data();
+    //     const openaiKey = key.key;
