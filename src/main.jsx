3213dbced5c1d7497b28aa8c092ac3cdfaa8c767
Joseph Martinez <joseph.r.martinez@gmail.com>
2023-05-09 17:46:37 -0700

    Delete App import on main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {
   createBrowserRouter,
@@ -40,6 +39,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
-
-
-// <App />
