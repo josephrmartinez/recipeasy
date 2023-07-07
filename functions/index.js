5dd1bf8530b6cb98a5a683dc060008d3ced4e019
Joseph Martinez <joseph.r.martinez@gmail.com>
2023-07-06 21:06:24 -0700

    Reinitialize firestore and functions

--- functions/index.js
+++ functions/index.js
@@ -1,34 +1,19 @@
-const functions = require('firebase-functions');
-const fetch = require('node-fetch');
-const { getDownloadURL, ref, uploadBytes } = require('firebase/storage');
-const { getStorage } = require('firebase/storage');
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
 
-// The Firebase Admin SDK to access Firestore.
-const admin = require("firebase-admin");
-admin.initializeApp();
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
 
-
-
-exports.imageProxy = functions.https.onRequest(async (req, res) => {
-  const imageUrlParam = req.query.imageUrl;
-
-  try {
-    // Download the image from the URL
-    const imageResponse = await fetch(imageUrlParam);
-    const imageData = await imageResponse.buffer();
-
-    // Upload the image to Firebase Cloud Storage
-    const storageRef = ref(storage, 'images/recipe.jpg');
-    await uploadBytes(storageRef, imageData);
-
-    // Get the download URL of the stored image
-    const downloadUrl = await getDownloadURL(storageRef);
-
-    // Return the Firebase Storage URL as the response
-    res.json({ imageUrl: downloadUrl });
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
